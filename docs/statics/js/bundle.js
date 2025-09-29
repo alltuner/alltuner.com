7899d7836ac84d8713d867d088079e00c4b2aa9c
David Poblador i Garcia <david@poblador.com>
2025-09-29 12:14:48 +0200

    update bundles

--- docs/statics/js/bundle.js
+++ docs/statics/js/bundle.js
@@ -1,7 +1,127 @@
-(()=>{var l={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};var x=([e,o,a])=>{let t=document.createElementNS("http://www.w3.org/2000/svg",e);return Object.keys(o).forEach(r=>{t.setAttribute(r,String(o[r]))}),a?.length&&a.forEach(r=>{let f=x(r);t.appendChild(f)}),t},i=(e,o={})=>{let a="svg",t={...l,...o};return x([a,t,e])};var C=e=>Array.from(e.attributes).reduce((o,a)=>(o[a.name]=a.value,o),{}),S=e=>typeof e=="string"?e:!e||!e.class?"":e.class&&typeof e.class=="string"?e.class.split(" "):e.class&&Array.isArray(e.class)?e.class:"",h=e=>e.flatMap(S).map(a=>a.trim()).filter(Boolean).filter((a,t,r)=>r.indexOf(a)===t).join(" "),g=e=>e.replace(/(\w)(\w*)(_|-|\s*)/g,(o,a,t)=>a.toUpperCase()+t.toLowerCase()),s=(e,{nameAttr:o,icons:a,attrs:t})=>{let r=e.getAttribute(o);if(r==null)return;let f=g(r),u=a[f];if(!u)return console.warn(`${e.outerHTML} icon name was not found in the provided icons object.`);let d=C(e),p={...l,"data-lucide":r,...t,...d},m=h(["lucide",`lucide-${r}`,d,t]);m&&Object.assign(p,{class:m});let c=i(u,p);return e.parentNode?.replaceChild(c,e)};var n=({icons:e={},nameAttr:o="data-lucide",attrs:a={}}={})=>{if(!Object.values(e).length)throw new Error(`Please provide an icons object.
-If you want to use all the icons you can import it like:
- \`import { createIcons, icons } from 'lucide';
-lucide.createIcons({icons});\``);if(typeof document>"u")throw new Error("`createIcons()` only works in a browser environment.");let t=document.querySelectorAll(`[${o}]`);if(Array.from(t).forEach(r=>s(r,{nameAttr:o,icons:e,attrs:a})),o==="data-lucide"){let r=document.querySelectorAll("[icon-name]");r.length>0&&(console.warn("[Lucide] Some icons were found with the now deprecated icon-name attribute. These will still be replaced for backwards compatibility, but will no longer be supported in v1.0 and you should switch to data-lucide"),Array.from(r).forEach(f=>s(f,{nameAttr:"icon-name",icons:e,attrs:a})))}};n({icons:{}});})();
+(() => {
+  // node_modules/lucide/dist/esm/defaultAttributes.js
+  var defaultAttributes = {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    viewBox: "0 0 24 24",
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-width": 2,
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+  };
+
+  // node_modules/lucide/dist/esm/createElement.js
+  var createSVGElement = ([tag, attrs, children]) => {
+    const element = document.createElementNS("http://www.w3.org/2000/svg", tag);
+    Object.keys(attrs).forEach((name) => {
+      element.setAttribute(name, String(attrs[name]));
+    });
+    if (children?.length) {
+      children.forEach((child) => {
+        const childElement = createSVGElement(child);
+        element.appendChild(childElement);
+      });
+    }
+    return element;
+  };
+  var createElement = (iconNode, customAttrs = {}) => {
+    const tag = "svg";
+    const attrs = {
+      ...defaultAttributes,
+      ...customAttrs
+    };
+    return createSVGElement([tag, attrs, iconNode]);
+  };
+
+  // node_modules/lucide/dist/esm/replaceElement.js
+  var getAttrs = (element) => Array.from(element.attributes).reduce((attrs, attr) => {
+    attrs[attr.name] = attr.value;
+    return attrs;
+  }, {});
+  var getClassNames = (attrs) => {
+    if (typeof attrs === "string") return attrs;
+    if (!attrs || !attrs.class) return "";
+    if (attrs.class && typeof attrs.class === "string") {
+      return attrs.class.split(" ");
+    }
+    if (attrs.class && Array.isArray(attrs.class)) {
+      return attrs.class;
+    }
+    return "";
+  };
+  var combineClassNames = (arrayOfClassnames) => {
+    const classNameArray = arrayOfClassnames.flatMap(getClassNames);
+    return classNameArray.map((classItem) => classItem.trim()).filter(Boolean).filter((value, index, self) => self.indexOf(value) === index).join(" ");
+  };
+  var toPascalCase = (string) => string.replace(/(\w)(\w*)(_|-|\s*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
+  var replaceElement = (element, { nameAttr, icons, attrs }) => {
+    const iconName = element.getAttribute(nameAttr);
+    if (iconName == null) return;
+    const ComponentName = toPascalCase(iconName);
+    const iconNode = icons[ComponentName];
+    if (!iconNode) {
+      return console.warn(
+        `${element.outerHTML} icon name was not found in the provided icons object.`
+      );
+    }
+    const elementAttrs = getAttrs(element);
+    const iconAttrs = {
+      ...defaultAttributes,
+      "data-lucide": iconName,
+      ...attrs,
+      ...elementAttrs
+    };
+    const classNames = combineClassNames(["lucide", `lucide-${iconName}`, elementAttrs, attrs]);
+    if (classNames) {
+      Object.assign(iconAttrs, {
+        class: classNames
+      });
+    }
+    const svgElement = createElement(iconNode, iconAttrs);
+    return element.parentNode?.replaceChild(svgElement, element);
+  };
+
+  // node_modules/lucide/dist/esm/lucide.js
+  var createIcons = ({
+    icons = {},
+    nameAttr = "data-lucide",
+    attrs = {},
+    root = document
+  } = {}) => {
+    if (!Object.values(icons).length) {
+      throw new Error(
+        "Please provide an icons object.\nIf you want to use all the icons you can import it like:\n `import { createIcons, icons } from 'lucide';\nlucide.createIcons({icons});`"
+      );
+    }
+    if (typeof root === "undefined") {
+      throw new Error("`createIcons()` only works in a browser environment.");
+    }
+    const elementsToReplace = root.querySelectorAll(`[${nameAttr}]`);
+    Array.from(elementsToReplace).forEach(
+      (element) => replaceElement(element, { nameAttr, icons, attrs })
+    );
+    if (nameAttr === "data-lucide") {
+      const deprecatedElements = root.querySelectorAll("[icon-name]");
+      if (deprecatedElements.length > 0) {
+        console.warn(
+          "[Lucide] Some icons were found with the now deprecated icon-name attribute. These will still be replaced for backwards compatibility, but will no longer be supported in v1.0 and you should switch to data-lucide"
+        );
+        Array.from(deprecatedElements).forEach(
+          (element) => replaceElement(element, { nameAttr: "icon-name", icons, attrs })
+        );
+      }
+    }
+  };
+
+  // config/esbuild.config.js
+  createIcons({
+    icons: {
+      // Include a Comma Separated List of Icons
+    }
+  });
+})();
 /*! Bundled license information:
 
 lucide/dist/esm/defaultAttributes.js:
@@ -9,7 +129,7 @@ lucide/dist/esm/createElement.js:
 lucide/dist/esm/replaceElement.js:
 lucide/dist/esm/lucide.js:
   (**
-   * @license lucide v0.507.0 - ISC
+   * @license lucide v0.544.0 - ISC
    *
    * This source code is licensed under the ISC license.
    * See the LICENSE file in the root directory of this source tree.
